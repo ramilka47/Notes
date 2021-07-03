d84900144697dfa575a7fb89e65a5c51ce05caa3
ramil <aminevri@yandex.ru>
2021-07-03 13:11:04 +0500

    dialog menu action

--- app/src/main/java/com/ramil/notes/navigation/Screens.kt
+++ app/src/main/java/com/ramil/notes/navigation/Screens.kt
@@ -1,9 +1,11 @@
 package com.ramil.notes.navigation
 
 import android.content.Intent
+import androidx.fragment.app.Fragment
 import com.github.terrakok.cicerone.androidx.ActivityScreen
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 import com.ramil.notes.ui.MainActivity
+import com.ramil.notes.ui.dialogs.MenuActionsDialog
 import com.ramil.notes.ui.fragments.LoginFragment
 import com.ramil.notes.ui.fragments.NoteFragment
 import com.ramil.notes.ui.fragments.NotesFragment
@@ -24,6 +26,12 @@ object Screens {
 
     fun note() = FragmentScreen{
         NoteFragment()
+    }
+
+    fun actions(onResult : Fragment) = FragmentScreen{
+        MenuActionsDialog().apply {
+            setTargetFragment(onResult, MenuActionsDialog.REQUEST_CODE)
+        }
     }
 
 }
---
+++ app/src/main/java/com/ramil/notes/ui/dialogs/MenuActionsDialog.kt
@@ -0,0 +1,16 @@
+package com.ramil.notes.ui.dialogs
+
+import androidx.fragment.app.DialogFragment
+
+class MenuActionsDialog : DialogFragment() {
+
+    companion object{
+        const val REQUEST_CODE = 11
+        const val DELETE_NOTE = 12
+        const val MARK_OF_FINAL = 13
+        const val CLEAR_MARK_OF_FINAL = 14
+        const val ADD_IMAGE = 15
+        const val DELETE_IMAGE = 16
+    }
+
+}
