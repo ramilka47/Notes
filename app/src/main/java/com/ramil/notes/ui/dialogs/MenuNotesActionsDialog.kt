e38ef74e322a773f80e97c88249221fc3f8358aa
ramil <aminevri@yandex.ru>
2021-07-03 13:22:00 +0500

    notes action dialog

---
+++ app/src/main/java/com/ramil/notes/ui/dialogs/MenuNotesActionsDialog.kt
@@ -0,0 +1,13 @@
+package com.ramil.notes.ui.dialogs
+
+import androidx.fragment.app.DialogFragment
+
+class MenuNotesActionsDialog : DialogFragment() {
+
+    companion object{
+        const val REQUEST_CODE = 21
+        const val ADD_NOTE = 22
+        const val EXIT = 23
+    }
+
+}
